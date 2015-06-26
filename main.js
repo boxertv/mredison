983f8adba9fb36d52dedae5e476b5ed261fadcae
Gergely Imreh <imrehg@gmail.com>
2015-06-26 20:49:59 +0800

    update add LCD demo to the main code

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var mraa = require('mraa'); //require mraa
 console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to resin.io dashboard logs
 
+function char(x) { return parseInt(x, 16); }
+
 var myOnboardLed = new mraa.Gpio(5); //LED hooked up to Groove Board D5
 myOnboardLed.dir(mraa.DIR_OUT); //set the gpio direction to output
 var ledState = true; //Boolean to hold the state of Led
@@ -14,7 +16,13 @@ function periodicActivity()
   setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
 }
 
+// From https://github.com/intel-iot-devkit/mraa/blob/v0.7.2/examples/javascript/rgblcd.js
+lcd = new m.I2c(0)
 
+lcd.address(0x62)
+lcd.writeReg(0, 0)
+lcd.writeReg(1, 0)
 
-
-
+lcd.writeReg(char('0x08'), char('0xAA'))
+lcd.writeReg(char('0x04'), 255)
+lcd.writeReg(char('0x02'), 255)
