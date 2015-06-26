387804bb1ebfd5d627369ff96ad6bcda68087d01
Gergely Imreh <imrehg@gmail.com>
2015-06-27 00:39:03 +0800

    add lcd rgb helper

--- main.js
+++ main.js
@@ -16,28 +16,36 @@ function periodicActivity()
   setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
 }
 
+var display = require('intel-edison-lcd-rgb-backlight-display-helper');
+// Set display rows/cols
+display.set(2, 16);
+// Set color
+display.setColor('blue');
+// Display keyword on row 1
+display.write('HELLO!');
+
 // From https://github.com/intel-iot-devkit/mraa/blob/v0.7.2/examples/javascript/rgblcd.js
-lcd = new mraa.I2c(0)
+// lcd = new mraa.I2c(0)
 
-lcd.address(0x62)
-lcd.writeReg(0, 0)
-lcd.writeReg(1, 0)
+// lcd.address(0x62)
+// lcd.writeReg(0, 0)
+// lcd.writeReg(1, 0)
 
-lcd.writeReg(char('0x08'), char('0x88'))
-lcd.writeReg(char('0x04'), 255)
-lcd.writeReg(char('0x02'), 255)
+// lcd.writeReg(char('0x08'), char('0x88'))
+// lcd.writeReg(char('0x04'), 255)
+// lcd.writeReg(char('0x02'), 255)
 
-periodicActivity();
+// periodicActivity();
 
-ledval = 97
+// ledval = 97
 
-function periodicLCDActivity()
-{
-    lcd.write(char('0x40'), ledval)
-    console.log(ledval);
-    ledval++;
-    if (ledval > 126) {
-	ledval = 97;
-    }
-    setTimeout(periodicLCDActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
-}
+// function periodicLCDActivity()
+// {
+//     lcd.write(char('0x40'), ledval)
+//     console.log(ledval);
+//     ledval++;
+//     if (ledval > 126) {
+// 	ledval = 97;
+//     }
+//     setTimeout(periodicLCDActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
+// }
