466592f52420da51176196e412328d1b1889cba2
Gergely Imreh <imrehg@gmail.com>
2015-07-30 14:19:47 +0000

    Working configuration on the default Edison Yocto

--- mredison/mredison.py
+++ mredison/mredison.py
@@ -2,25 +2,27 @@ import mraa
 import time
 import pyupm_i2clcd as lcd
 
-myLcd = lcd.Jhd1313m1(6, 0x3E, 0x62)
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
 # Clear
 myLcd.clear()
 
-# Green
-myLcd.setColor(255, 255, 0)
+# A kinda blue
+myLcd.setColor(0x30, 0x2F, 0xFF)
 
-# Zero the cursor
-myLcd.setCursor(0,0)
-
-# Print it.
-myLcd.write("What's up?");
-
-x = mraa.Gpio(7)
+x = mraa.Gpio(4)
 x.dir(mraa.DIR_OUT)
 
-while True:
-    x.write(1)
-    time.sleep(0.2)
-    x.write(0)
-    time.sleep(0.2)
+period = 0.15
+ledvalue = 1
+
+long_string = ' '*15 + 'Hello Mr. Edison, what\'s going on?' + ' '*15
+while(1):
+    for i in range(len(long_string) - 16 + 1):
+         framebuffer = long_string[i:i+16]
+         myLcd.setCursor(0,0)
+         myLcd.write(framebuffer)
+         print(framebuffer)
+         x.write(ledvalue)
+         ledvalue = 1 - ledvalue
+         time.sleep(period)
