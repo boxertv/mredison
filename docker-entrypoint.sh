aad0b44d4a67a4be56bbf8ed79a49334fe321994
Gergely Imreh <imrehg@gmail.com>
2015-08-02 19:59:51 +0800

    double mount and sleep is probably not helping, remove them

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,4 @@
 
 ## Mount needed for GPIO pins to be enabled correctly
 mount -t debugfs nodev /sys/kernel/debug && \
-sleep 5 && \
-mount -t debugfs nodev /sys/kernel/debug && \
-sleep 5 && \
 python /mredison/mredison.py
