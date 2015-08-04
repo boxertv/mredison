d8f153438edcca12bdbed994315f28c403f54ea2
Gergely Imreh <imrehg@gmail.com>
2015-08-04 23:19:31 +0800

    Revert "reduce commands for gpio"
    This reverts commit 80cb84ae7a043de2483da091f5866317636a93f5.
    
    Looks like it does not work, at least not in this form, after
    a couple of boots.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-if mount -l -t debugfs | grep "on /sys/kernel/debug"; then
-    echo "debugfs already mounted"
-else
-    mount -t debugfs nodev /sys/kernel/debug
-fi
-
-udevadm trigger
-
+./gpio_setup.sh && \
 python /mredison/mredison.py
