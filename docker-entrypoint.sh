58eb0568a6785e7382e510034a48247efb588940
Gergely Imreh <imrehg@gmail.com>
2015-08-12 21:47:20 +0800

    try different GPIO setup trigger

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-./gpio_setup.sh && \
+# Based on: http://talk.resin.io/t/libusb0-py-cannot-open-a-directory/62/16
+
+# Mount needed for GPIO pins to be enabled correctly
+if mount -l -t debugfs | grep "on /sys/kernel/debug"; then
+    echo "debugfs already mounted"
+else
+    mount -t debugfs nodev /sys/kernel/debug
+fi
+
+udevd & udevadm trigger
+
 python /mredison/mredison.py
