d8df3553a516b9e1a788a7243647d7228a25a484
Gergely Imreh <imrehg@gmail.com>
2015-08-13 10:40:19 +0800

    Revert "try different GPIO setup trigger"
    This reverts commit 58eb0568a6785e7382e510034a48247efb588940.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# Based on: http://talk.resin.io/t/libusb0-py-cannot-open-a-directory/62/16
-
-# Mount needed for GPIO pins to be enabled correctly
-if mount -l -t debugfs | grep "on /sys/kernel/debug"; then
-    echo "debugfs already mounted"
-else
-    mount -t debugfs nodev /sys/kernel/debug
-fi
-
-udevd & udevadm trigger
-
+./gpio_setup.sh && \
 python /mredison/mredison.py
