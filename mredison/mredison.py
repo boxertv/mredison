1af7f6ca08a371c4cca5e0be8e2c12a4588f7b7f
Gergely Imreh <imrehg@gmail.com>
2015-08-29 16:12:28 +0800

    buzzer changes

--- mredison/mredison.py
+++ mredison/mredison.py
@@ -29,7 +29,7 @@ led.dir(mraa.DIR_OUT)
 
 # using GPIO pin 5, has to be a PWM capable pin
 buzzer = upmBuzzer.Buzzer(5)
-buzzerVolume = 0.25
+buzzerVolume = 0.125
 buzzer.setVolume(0)
 
 ###### End setting up
@@ -108,19 +108,22 @@ def ledblink(display):
 
 notes = { "X1": 1010, # 990Hz
           "X2": 1133, # 882Hz
-          "X3": 852, # 1171Hz
-          "X4": 1351, # 740Hz
-          "X5": 1052, # 950hz
+          "X3": 847, # 1180Hz
+          "X4": 1250, # 800Hz
+          "X5": 1052, # 950Hz
+          "X6": 1280, # 780Hz
+          "X7": 1333, # 750Hz
+          "X8": 1052, # 950Hz
          }
 def playGTA():
     tempo = 60 * 1000
     pager = [("X1", 5), ("X1", 5), ("X1", 1), (" ", 4),
              ("X2", 2), ("X1", 2), ("X3", 2), ("X1", 2), (" ", 2),
-             ("X2", 2), ("X2", 5), (" ", 4),
-             ("X1", 2), (" ", 2),
-             ("X4", 5), ("X4", 1), (" ", 1),
-             ("X4", 5), ("X4", 5), ("X1", 1), (" ", 1),
-             ("X1", 5), ("X5", 5)]
+             ("X2", 2), ("X1", 5), (" ", 4),
+             ("X4", 2), (" ", 2),
+             ("X6", 5), ("X4", 1), (" ", 1),
+             ("X7", 5), ("X7", 5), ("X7", 1), (" ", 1),
+             ("X2", 5), ("X5", 5)]
     playMelody(pager, tempo)
 
 def playMelody(melody, tempo):
@@ -129,13 +132,13 @@ def playMelody(melody, tempo):
         note, beat = m
         if note == " ":
             buzzer.setVolume(0.0)
-            buzzer.playSound(3000, beat * tempo)
+            buzzer.playSound(1000, beat * tempo)
             buzzer.setVolume(buzzerVolume)
         else:
             buzzer.playSound(notes[note], beat * tempo)
         # add a bit of trailing break
         buzzer.setVolume(0.0)
-        buzzer.playSound(3000, int(0.1 * tempo))
+        buzzer.playSound(1000, int(0.1 * tempo))
         buzzer.setVolume(buzzerVolume)
 
 class TestBot(irc.bot.SingleServerIRCBot):
